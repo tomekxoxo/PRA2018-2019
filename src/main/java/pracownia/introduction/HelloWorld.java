c210bbb03ac9f2ef048dbeb67f4fe094ae81001d
Tomasz Kasprowicz <tomasz.kasprowicz@rsqtechnologies.com>
2021-03-13 17:08:31 +0100

    commit1

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,15 @@
 package pracownia.introduction;
 
+//import java.util.logging.Logger;
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+    final static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String [ ] args) {
-        System.out.println("Hello World");
+
+        System.out.println("Hello bitches");
+
+        logger.info("Logger is running");
     }
 }
